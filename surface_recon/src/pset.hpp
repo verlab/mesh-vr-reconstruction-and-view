48bec1b210f6f5053b40555b71351e2d78135895
guipotje <gui_potje@hotmail.com>
2020-07-02 15:06:41 -0300

    Add license warning to each file

--- surface_recon/src/pset.hpp
+++ surface_recon/src/pset.hpp
@@ -1,3 +1,21 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%   This file is part of https://github.com/verlab/mesh-vr-reconstruction-and-view
+//
+//   mesh-vr-reconstruction-and-view is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   mesh-vr-reconstruction-and-view is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with mesh-vr-reconstruction-and-view.  If not, see <http://www.gnu.org/licenses/>.
+//%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 #ifndef _PLY_PARSER_
 #define _PLY_PARSER_
 
